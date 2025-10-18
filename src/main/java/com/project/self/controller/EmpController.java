65a9a3b73b42e6dc495ad740a765fd398d53df08
Hamza <hhasan2605@gmail.com>
2025-10-19 03:31:28 +0530

    added update employee

--- src/main/java/com/project/self/controller/EmpController.java
+++ src/main/java/com/project/self/controller/EmpController.java
@@ -2,13 +2,12 @@ package com.project.self.controller;
 
 import com.project.self.Model.Employee;
 import com.project.self.service.EmpService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EmpController {
@@ -28,6 +27,24 @@ public class EmpController {
     public void CreateEmployee(@RequestBody Employee employee){
         empService.addEmployee(employee);
         //System.out.println("added");
+    }
+
+    @PutMapping("/api/{id}")
+    public ResponseEntity<Employee> updateByid(@PathVariable Long id, @RequestBody Employee newemployee){
+            Employee old =empService.getById(id).orElse(null);
+
+            if(old!=null){
+                old.setName(newemployee.getName()!=null && newemployee.getName()!=""?newemployee.getName(): old.getName());
+                old.setEmail(newemployee.getEmail()!=null && newemployee.getEmail()!=""?newemployee.getEmail(): old.getEmail());
+                old.setRole(newemployee.getRole()!=null && newemployee.getRole()!=""?newemployee.getRole(): old.getRole());
+                old.setLocation(newemployee.getLocation()!=null && newemployee.getLocation()!=""?newemployee.getLocation(): old.getLocation());
+                old.setSalary(newemployee.getSalary()!=null?newemployee.getSalary(): old.getSalary());
+                empService.saveAll(old);
+                return new ResponseEntity<>(old, HttpStatus.CREATED);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+
     }
 
 
--- src/main/java/com/project/self/service/EmpService.java
+++ src/main/java/com/project/self/service/EmpService.java
@@ -5,6 +5,7 @@ import com.project.self.repo.EmpRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmpService {
@@ -17,9 +18,19 @@ public class EmpService {
     public List<Employee> getAll(){
         return empRepo.findAll();
     }
+    public Optional<Employee> getById(Long id){
+        return empRepo.findById(id);
+    }
+
+    public Employee saveAll(Employee employee){
+        return empRepo.save(employee);
+
+    }
 
     public Employee addEmployee(Employee employee){
         return empRepo.save(employee);
     }
+
+
 
 }
